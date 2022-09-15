7085e9c18273a05d5b735d71c0a696cceb1db204
Joentze <Joentze01@gmail.com>
2022-09-15 19:08:02 +0800

    feat: added dashboard html and websocket connection

--- micro_serial.py
+++ micro_serial.py
@@ -1,6 +1,25 @@
+import asyncio
 import serial
+import websockets
 PORT_NAME = "/dev/tty.usbmodem2402"
 ser = serial.Serial(PORT_NAME, 9600)
-while True:
-     cc=str(ser.readline())
-     print(cc[2:][:-5])
+
+async def write_serial():
+     while True:
+          async with websockets.connect("ws://localhost:8001") as websocket:
+               cc=str(ser.readline())
+               await websocket.send(cc[2:][:-5])
+               print(cc[2:][:-5])
+
+async def time(websocket, path):
+	while True:
+		cc=str(ser.readline())[2:][:-5]
+		print(cc)
+		await websocket.send(cc)
+
+start_server = websockets.serve(time, '127.0.0.1', 5678)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
+#asyncio.run(write_serial())
