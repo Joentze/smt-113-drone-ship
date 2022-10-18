e4952e72f5a457c4277a4f723b66eee20af4b936
Joentze <Joentze01@gmail.com>
2022-10-18 18:21:42 +0800

    feat: send telegram message from dashboard

--- main.js
+++ main.js
@@ -14,3 +14,25 @@ var ws = new WebSocket("ws://127.0.0.1:5678/"),
                 // messages.appendChild(message);
             };
             // document.body.appendChild(messages);
+const getWeatherData = ()=>{
+    let xhr = new XMLHttpRequest()
+    xhr.open("GET", "https://api.data.gov.sg/v1/environment/air-temperature")
+    xhr.send()
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          console.log(xhr.response);
+        }
+      }
+}
+const sendTelegramMsg = ()=>{
+    let xhr = new XMLHttpRequest()
+    xhr.open("POST", "https://api.telegram.org/bot5498520650:AAEaMWPf4UDpPGh65jIeedu2vezhUXfrWXE/sendMessage")
+    xhr.setRequestHeader('Content-Type','application/json')
+    xhr.send(JSON.stringify({"chat_id":"549991017","text":"hello"}))    
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          console.log(xhr.response);
+        }
+      }
+}
+
